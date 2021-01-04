c2d82f436f5d246505bb4a6286541877d0892041
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-01-04 14:37:19 -0500

    add gh-pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import Popper from 'popper.js';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
